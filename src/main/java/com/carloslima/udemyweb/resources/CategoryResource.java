e5f436b9409ec4cd84030538e912814ccfbef4a9
carloslimadev <carloscmbl20@gmail.com>
2020-01-02 23:51:11 +0000

    updated list restful service

--- src/main/java/com/carloslima/udemyweb/resources/CategoryResource.java
+++ src/main/java/com/carloslima/udemyweb/resources/CategoryResource.java
@@ -1,8 +1,13 @@
 package com.carloslima.udemyweb.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.carloslima.udemyweb.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -10,8 +15,16 @@ public class CategoryResource {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "list categories rest service is work";
+	public List<Category> list() {
+	
+	Category cat1 = new Category(1, null,"tecnology");
+	Category cat2 = new Category(2, null,"tecnology");
+	
+	List<Category> categories = new ArrayList<>();
+	categories.add(cat1);
+	categories.add(cat2);
+	return categories;
+		//return "list categories rest service is work";
 	}
 
 }

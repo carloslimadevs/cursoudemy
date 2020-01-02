36167eeaca559dc2ddadf0a3284d68a1a2c3bd2a
carloslimadev <carloscmbl20@gmail.com>
2020-01-02 23:16:22 +0000

    project started

---
+++ src/main/java/com/carloslima/udemyweb/UdemywebApplication.java
@@ -0,0 +1,13 @@
+package com.carloslima.udemyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemywebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloslima/udemyweb/UdemywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloslima.udemyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

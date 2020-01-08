689c4fb900d4043cf13c9c4d9429877e2fdab04a
carloslimadev <carloscmbl20@gmail.com>
2020-01-08 15:18:22 +0000

    create and save produts and associations (categories) and vice-versa

--- src/main/java/com/carloslima/udemyweb/UdemywebApplication.java
+++ src/main/java/com/carloslima/udemyweb/UdemywebApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.carloslima.udemyweb.domain.Category;
+import com.carloslima.udemyweb.domain.Product;
 import com.carloslima.udemyweb.repositories.CategoryRepository;
+import com.carloslima.udemyweb.repositories.ProductRepository;
 
 @SpringBootApplication
 public class UdemywebApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UdemywebApplication.class, args);
@@ -26,8 +30,19 @@ public class UdemywebApplication implements CommandLineRunner{
 		Category categoryOne = new Category(null, "office description", "Office");
 		Category categoryTwo = new Category(null,"Tecnology description", "Tecnology");
 		
-		categoryRepository.saveAll(Arrays.asList(categoryOne,categoryTwo));
+		Product productOne = new Product(null, "Computer", 2000.00);
+		Product productTwo = new Product (null,"Printer",400.00);
+		Product productThree = new Product(null,"chair",40.00);
 		
+		categoryOne.getProducts().addAll(Arrays.asList(productOne,productTwo,productThree));
+		categoryTwo.getProducts().add(productTwo);
+		
+		productOne.getCategories().add(categoryOne);
+		productTwo.getCategories().addAll(Arrays.asList(categoryOne,categoryTwo));
+		productThree.getCategories().add(categoryOne);
+		
+		categoryRepository.saveAll(Arrays.asList(categoryOne,categoryTwo));
+		productRepository.saveAll(Arrays.asList(productOne,productTwo,productThree));
 	}
 
 }
